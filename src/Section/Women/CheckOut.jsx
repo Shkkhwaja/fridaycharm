8700beddd26bad4c0316ad93f45b585b3d5c923b
Khwaja Shaikh <109279460+Shkkhwaja@users.noreply.github.com>
2024-07-02 16:58:24 +0530

    fixed error or add to cart

--- src/Section/Women/CheckOut.jsx
+++ src/Section/Women/CheckOut.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useCart } from "react-use-cart";
+
 
 export default function CheckOut() {
+
+  const {
+    isEmpty,
+    totalUniqueItems,
+    items,
+    totalItems,
+    cartTotal,
+    updateItemQuantity,
+    removeItem,
+    emptyCart,
+  } = useCart();
   return (
     <div className='h-screen w-full bg-gray-600'>
-      <h2>Check Out</h2>
+      <h2>Check Out : {cartTotal}</h2>
     </div>
   )
 }
